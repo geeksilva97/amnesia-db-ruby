a87ca516af6ceb3b2977ced2fdc501e15b076e78
Edigleysson Silva <edigleyssonsilva@gmail.com>
2023-04-05 22:15:31 -0300

    add new rule to the parser

--- lib/amnesia/query_parser.rb
+++ lib/amnesia/query_parser.rb
@@ -2,10 +2,13 @@ module Amnesia
   class QueryParser
     def self.call(raw_command)
       tokens = raw_command.split(' ')
+      puts tokens
 
-      raise 'Unable to parse command' if tokens.size > 3
+      raise 'Unable to parse command' if tokens.size > 3 || tokens.size < 2
 
-      tokens
+      instruction, *rest = tokens
+
+      [instruction.downcase, *rest]
     end
   end
 end
