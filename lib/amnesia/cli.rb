e859438a0b90837d905e4fba671dd4e4b9fa7461
Edigleysson Silva <edigleyssonsilva@gmail.com>
2023-04-05 22:06:01 -0300

    refactor cli

--- lib/amnesia/cli.rb
+++ lib/amnesia/cli.rb
@@ -1,26 +1,36 @@
 require_relative './query_parser'
+require_relative './query_analyzer'
 require_relative './query_runner'
 
 module Amnesia
   class CLI
     def start
+      query_runner = Amnesia::QueryRunner.new
+      puts "Welcome to AmnesiaDB - Version 0.0.1\n\n"
+
       loop do
-        puts "Welcome to AmnesiaDB - Version 0.0.1\n\n"
+        print '> '
         raw_command = gets.delete("\n")
 
         break if raw_command == '.exit'
 
         instruction_keyword, key, value = Amnesia::QueryParser.call(raw_command)
 
-        #         if instruction.nil?
-        #           puts "Unknown command\n\n"
-        #           next
-        #         end
+        next if analysis_failed?(instruction_keyword, key, value)
 
-        result = Amnesia::QueryRunner.call(instruction_keyword, key, value)
+        result = query_runner.run(instruction_keyword, key, value)
 
         puts "#{result}\n\n"
       end
+    end
+
+    private
+
+    def analysis_failed?(instruction_keyword, key, value)
+      Amnesia::QueryAnalyzer.call!(instruction_keyword, key, value)
+    rescue RuntimeError => e
+      puts "Error: #{e}\n\n"
+      true
     end
   end
 end
