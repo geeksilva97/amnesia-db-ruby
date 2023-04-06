8e897b67399fcc35b42a93744edfd8f41a2b9e22
Edigleysson Silva <edigleyssonsilva@gmail.com>
2023-04-05 22:15:41 -0300

    add new rule to the analyzer

--- lib/amnesia/query_analyzer.rb
+++ lib/amnesia/query_analyzer.rb
@@ -1,8 +1,9 @@
 module Amnesia
   class QueryAnalyzer
-    def self.call!(instruction, key, _value)
+    def self.call!(instruction, key, value)
       raise 'unknown instruction' unless instruction_grammar.include? instruction
       raise 'malformed key' unless key_valid?(key)
+      raise 'set command must contain a key and a value' if instruction == 'set' && value.nil?
     end
 
     def self.instruction_grammar
