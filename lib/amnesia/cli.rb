f2e3b1339f993465ccfa2c6e46c589ae0311bdce
Edigleysson Silva <edigleyssonsilva@gmail.com>
2023-04-10 22:14:34 -0300

    allow cli to receive a filename

--- lib/amnesia/cli.rb
+++ lib/amnesia/cli.rb
@@ -1,16 +1,22 @@
 require_relative './query_parser'
 require_relative './query_analyzer'
 require_relative './query_runner'
+require_relative './storage'
 
 module Amnesia
   class CLI
+    def initialize(filename)
+      @amnesia_storage = Amnesia::Storage.new(filename)
+      @amnesia_storage.create_db_file unless @amnesia_storage.file_exists?
+    end
+
     def start
-      query_runner = Amnesia::QueryRunner.new
+      query_runner = Amnesia::QueryRunner.new(@amnesia_storage)
       puts "Welcome to AmnesiaDB - Version 0.0.1\n\n"
 
       loop do
         print '> '
-        raw_command = gets.delete("\n")
+        raw_command = $stdin.gets.delete("\n")
 
         break if raw_command == '.exit'
 
