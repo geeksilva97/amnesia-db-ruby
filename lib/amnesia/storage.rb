bd3f70be60a0bdba93a0b5e8593629185bfe4a6b
Edigleysson Silva <edigleyssonsilva@gmail.com>
2023-04-10 23:27:12 -0300

    implement index reading

--- lib/amnesia/storage.rb
+++ lib/amnesia/storage.rb
@@ -9,20 +9,40 @@ module Amnesia
       offset = File.size(filename)
       entry = "#{key},#{value}\n"
 
-      @index_structure[key] = [offset, entry.bytesize]
+      @index_structure[key] = [offset, entry.bytesize - 1]
 
       File.write(filename, entry, mode: 'a+')
     end
 
     def get(key)
+      index_entry = @index_structure[key]
+
+      return record_from_index(index_entry) unless index_entry.nil?
+
+      record_from_scan(key)
+    end
+
+    def record_from_scan(key)
       lines = File.readlines(filename)
 
-      result = lines.filter do |line|
+      record = lines.filter do |line|
         record_key, = line.split(',', 2)
         record_key == key
-      end
+      end.last
 
-      result.last.split(',', 2)[1] unless result.empty?
+      parse_record(record) unless record.nil?
+    end
+
+    def record_from_index(index_entry)
+      offset, size = index_entry
+
+      record = File.read(filename, size, offset)
+
+      parse_record(record) unless record.nil?
+    end
+
+    def parse_record(raw_record)
+      raw_record.split(',', 2)[1]
     end
 
     def create_db_file
