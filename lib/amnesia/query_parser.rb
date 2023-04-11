6dc4e0e66cf4086e308d9c9c02013b2f2ad76fdc
Edigleysson Silva <edigleyssonsilva@gmail.com>
2023-04-10 22:29:19 -0300

    remove unused instructions

--- lib/amnesia/query_parser.rb
+++ lib/amnesia/query_parser.rb
@@ -2,7 +2,6 @@ module Amnesia
   class QueryParser
     def self.call(raw_command)
       tokens = raw_command.split(' ')
-      puts tokens
 
       raise 'Unable to parse command' if tokens.size > 3 || tokens.size < 2
 
