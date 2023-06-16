09c9108d1577afce6e18e15b974c94b84c7b14f2
Edigleysson Silva <edigleyssonsilva@gmail.com>
2023-06-16 00:01:00 -0300

    update storage tests

--- spec/amnesia/storage_spec.rb
+++ spec/amnesia/storage_spec.rb
@@ -19,10 +19,6 @@ RSpec.describe Amnesia::Storage do
       subject.set('key', 'value')
     end
 
-    it 'checks file size to calculate the offset' do
-      expect(File).to have_received(:size).with('amnesia.db')
-    end
-
     it 'writes key,value to the file' do
       expect(File).to have_received(:write).with('amnesia.db', "key,value\n", mode: 'a+')
     end
@@ -30,14 +26,16 @@ RSpec.describe Amnesia::Storage do
 
   describe '#get' do
     context 'when data is indexed' do
-      it 'reads from index' do
+      it 'reads using the given index entry' do
         allow(File).to receive(:read)
           .with('amnesia.db', 17, 0)
           .and_return('somekey,somevalue')
 
         subject.set('somekey', 'somevalue')
 
-        expect(subject.get('somekey')).to eq('somevalue')
+        result = subject.get('somekey', index_entry: [0, 17])
+
+        expect(result).to eq('somevalue')
         expect(File).not_to have_received(:readlines)
         expect(File).to have_received(:read)
       end
@@ -52,19 +50,6 @@ RSpec.describe Amnesia::Storage do
         expect(File).to have_received(:readlines)
         expect(File).not_to have_received(:read)
       end
-    end
-  end
-
-  describe '#populate_index' do
-    before do
-      allow(File).to receive(:readlines)
-        .and_return((1..2).map { |i| "key#{i},value#{i}" }.to_a)
-
-      subject.populate_index
-    end
-
-    it 'builds structcure from database file' do
-      expect(File).to have_received(:readlines).with('amnesia.db')
     end
   end
 end
