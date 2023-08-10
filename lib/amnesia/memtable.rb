babcd78895be4fe5ded5d4106bc9a416aef36068
Edigleysson Silva <edigleyssonsilva@gmail.com>
2023-08-09 21:02:12 -0300

    wipe memtable implementations

--- lib/amnesia/memtable.rb
+++ lib/amnesia/memtable.rb
@@ -3,21 +3,15 @@ module Amnesia
     attr_accessor :status
 
     def initialize
-      @store = Amnesia::Support::AVL.new
-      @status = :active
     end
 
     def read(key)
-      @store.find(key)
     end
 
     def write(key, value)
-      @store.insert(key, value)
     end
 
     def flush(segment_handler)
-      # TODO: Write a whole block
-      @store.traverse { |node| segment_handler.store({ key: node.key, value: node.value }) }
     end
   end
 end
--- lib/amnesia/memtable_handler.rb
+++ lib/amnesia/memtable_handler.rb
@@ -6,23 +6,17 @@ module Amnesia
     end
 
     def read(key)
-      memtable.read(key)
     end
 
     def write(key, value)
-      memtable.write(key, value)
     end
 
     def flush
-      memtable.status = :flushing
-      memtable.flush(segment_handler)
-      memtable.status = :finished_flusing
     end
 
     private
 
     def memtable
-      @memtables.first
     end
   end
 end
