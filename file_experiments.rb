bd6e894fbf0a55bcede22c17b94546d116b0eb99
Edigleysson Silva <edigleyssonsilva@gmail.com>
2023-09-01 03:03:45 -0300

    experiment on listing records

--- file_experiments.rb
+++ file_experiments.rb
@@ -1,25 +1,73 @@
 handle = File.open('./_data/sstable.bin')
 
-header = handle.read(13).unpack('CQL')
+num_keys, timestamp = handle.read(13).unpack('CQL')
 
-pp({ num_keys: header[0], created_at: Time.at(header[1]) })
+data_blocks = handle.read
+seek = 0
 
-block = handle.read
+# the layout is bad. Ruby does not know how to unpack variable Data
+# with slice we can skip bytes
 
-metadata = block.unpack('CQC') # pega os primeiros 10 bytes
+0.upto(num_keys - 1) do |index|
+  record_size_tombstone_flag, timestamp, key_size = data_blocks.unpack("@#{seek}CQC")
 
-# skippa 10 bytes
-key, value_size, value = block.unpack("@10a#{metadata[2]}Ca*")
+  record_size = record_size_tombstone_flag >> 1 # pegando os primeiros 7 bits
 
-pp key
-pp value_size
-pp value
+  is_tombstone = record_size_tombstone_flag & 1 # aplicando AND no bit menos significativo
 
-record_size = metadata[0] >> 1 # pegando os primeiros 7 bits
-is_tombstone = metadata[0] & 1 # aplicando AND no bit menos significativo
+  seek += 10
 
-pp block
+  key, = data_blocks.unpack("@#{seek}a#{key_size}")
 
-puts "Record size: #{record_size} / É tombstone? -> #{is_tombstone == 1} / Criado em #{Time.at(metadata[1])}"
+  seek += key_size
 
-handle.close
+  value_size, = data_blocks.unpack("@#{seek}C")
+
+  seek += 1
+
+  value,  = data_blocks.unpack("@#{seek}a#{value_size}")
+
+  seek += value_size
+
+  puts "#{index} Record details\n\nTombstone? -> #{is_tombstone == 1}\nKey -> #{key}\nKey Size ->#{key_size}\nValue size-> #{value_size}\nValue ->#{value}\nRecord size -> #{record_size}\n"
+end
+
+# seek += value_size + 8
+
+# pp value
+
+# 0.upto(num_keys - 1) do |index|
+#   puts "looking for record #{index}"
+#   record_size_tombstone_flag, timestamp, key_size = data_blocks.unpack("@#{seek}CQC")
+
+#   record_size = record_size_tombstone_flag[0] >> 1 # pegando os primeiros 7 bits
+#   is_tombstone = record_size_tombstone_flag[0] & 1 # aplicando AND no bit menos significativo
+
+#   seek += 10
+
+#   key = data_blocks.unpack("@#{seek}a#{key_size}")
+
+#   seek += key_size # incrementa a quantidade de bytes lidos para o valor da key
+
+#   puts "\nRecord size -> #{record_size_tombstone_flag}\nKey -> #{key}\nTombstone? -> #{is_tombstone == 1}"
+#   puts ''
+# end
+
+# metadata = block.unpack('CQC') # pega os primeiros 10 bytes
+
+# # skippa 10 bytes
+# # senti fala de uma snynaxe como a do perl. seria bem mais util no unpacking de dadoa variaveis
+# key, value_size, value = block.unpack("@10a#{metadata[2]}Ca*")
+
+# pp key
+# pp value_size
+# pp value
+
+# record_size = metadata[0] >> 1 # pegando os primeiros 7 bits
+# is_tombstone = metadata[0] & 1 # aplicando AND no bit menos significativo
+
+# pp block
+
+# puts "Record size: #{record_size} / É tombstone? -> #{is_tombstone == 1} / Criado em #{Time.at(metadata[1])}"
+
+# handle.close
